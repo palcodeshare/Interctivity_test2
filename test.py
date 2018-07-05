aa84da28e9397001a4d20a6afd2e23929e9af1ba
mokshaxkrodha <nilaykdoshi@gmail.com>
2018-07-05 15:04:01 +0400

    11

--- test.py
+++ test.py
@@ -1,20 +1,2 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, State, Output
-
-app = dash.Dash()
-app.layout = html.Div([
-    html.Div(id='target'),
-    dcc.Input(id='input', type='text', value=''),
-    html.Button(id='submit', n_clicks=0, children='Save')
-])
-
-@app.callback(Output('target', 'children'), [Input('submit', 'n_clicks')],
-              [State('input', 'value')])
-def callback(n_clicks, state):
-    return "callback received value: {}".format(state)
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+print(dash.__file__)
