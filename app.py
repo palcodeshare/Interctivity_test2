c878e8371637fe9a8a6fb60891c91ac5dca8ea8b
mokshaxkrodha <nilaykdoshi@gmail.com>
2018-07-08 10:35:59 +0400

    auth check 5

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ reg_val = [sales[0] for sales in region1]
 #Auth
 
 
-app = dash.Dash('app',server=server)
-auth = dash.Dash('auth')
+
+app = dash.Dash('auth')
 auth = dash_auth.BasicAuth(
     app,
     (('gfkdxb','1234',),)
@@ -43,6 +43,7 @@ auth = dash_auth.BasicAuth(
 
 #Dash app
 
+app = dash.Dash('app',server=server)
 
 app.layout = html.Div([
     html.Div([
