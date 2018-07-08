a38b2fe7fd4e91035f62005eb645040193657e4f
mokshaxkrodha <nilaykdoshi@gmail.com>
2018-07-08 12:49:01 +0400

    zip implementation

--- app.py
+++ app.py
@@ -36,12 +36,6 @@ cur.execute("SELECT DISTINCT(region) FROM react_table")
 region1=cur.fetchall()
 reg_val = [sales[0] for sales in region1]
 
-#Auth
-
-
-
-
-
 #Dash app
 
 app = dash.Dash('app',server=server)
@@ -73,17 +67,16 @@ app.layout = html.Div([
 
 def update_graph(reg_col_name):
 
-    print(reg_col_name)
-    SQL="SELECT fruits FROM react_table WHERE region = (%s)"
+    SQL="SELECT fruits, sales FROM react_table WHERE region = (%s)"
     cur.execute(SQL,(reg_col_name,))
-    fruits1=cur.fetchall()
-    fruits_val = [fruit[0] for fruit in fruits1]
-    SQL1="SELECT sales FROM react_table WHERE region = (%s) "
-    cur.execute(SQL1,(reg_col_name,))
-    sales1=cur.fetchall()
-    sales_val = [sales[0] for sales in sales1]
-    print(sales_val)
-    clo = conn.rollback()
+    result=cur.fetchall()
+    fruits_val, sales_val = zip(*result)
+    #SQL1="SELECT sales FROM react_table WHERE region = (%s) "
+    #cur.execute(SQL1,(reg_col_name,))
+    #sales1=cur.fetchall()
+    #sales_val = [sales[0] for sales in sales1]
+    #print(sales_val)
+    #clo = conn.rollback()
 
     return {
         'data': [go.Bar(
