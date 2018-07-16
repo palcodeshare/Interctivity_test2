3b3c23bf165b7bf214b9bd186e727ef186b714dd
mokshaxkrodha <nilaykdoshi@gmail.com>
2018-07-16 09:47:04 +0400

    dcc link troubleshoot

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ import plotly
 
 print(dcc.__version__)
 
-app = dash.Dash()
-server = app.server
-app.config.suppress_callback_exceptions = True
+#app = dash.Dash()
+#server = app.server
 
 #Bootstrap CSS
 #app.css.append_css({'external_url': 'https://codepen.io/mokshaxkrodha/pen/XBXNbP'})
@@ -31,6 +30,9 @@ server.secret_key = os.environ.get('secret_key', 'secret')
 #Dash app
 
 app = dash.Dash('app',server=server)
+
+app.config.suppress_callback_exceptions = True
+
 
 index_page = html.Div([
     dcc.Link('Go to Page 1', href='/apps/app1'),
