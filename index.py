c655004f2336b1a2f703ef7f492f1d4762a368aa
mokshaxkrodha <nilaykdoshi@gmail.com>
2018-11-04 11:43:35 +0400

    rr

--- index.py
+++ index.py
@@ -27,7 +27,7 @@ def display_page(pathname):
     elif pathname == '/apps/howtouse':
          return howtouse.layout
     else:
-        return '404'
+        return shelldashboard.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
