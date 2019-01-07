0c263800a0f51ebb6db338ae691f9b9640a3658d
mokshaxkrodha <nilaykdoshi@gmail.com>
2019-01-07 17:29:08 +0400

    cc

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from dash-auth-master import dash_auth
+import dash_auth
 from app import app
 from apps import shelldashboard,howtouse,notes
 import os
