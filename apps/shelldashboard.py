19b653c2e54ead92c1cc728eb8b149345aaf3b4a
mokshaxkrodha <nilaykdoshi@gmail.com>
2019-02-14 11:47:14 +0400

    Update shelldashboard.py

--- apps/shelldashboard.py
+++ apps/shelldashboard.py
@@ -1601,13 +1601,13 @@ def update_flag(ctry_name, typeveh_name, base_name):
     layout = go.Layout(
         title='Potential Headroom',
         barmode='stack',
-        margin=go.layout.Margin(
-            l=300,
-            r=50,
-            b=100,
-            t=100,
-            pad=4
-        ),
+        # margin=go.layout.Margin(
+        #     l=300,
+        #     r=50,
+        #     b=100,
+        #     t=100,
+        #     pad=4
+        # ),
         paper_bgcolor='#7f7f7f',
         plot_bgcolor='#c7c7c7'
     )
