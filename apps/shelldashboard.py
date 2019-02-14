1ce8033e3cbb7d40458349a27b347bb071b87077
mokshaxkrodha <nilaykdoshi@gmail.com>
2019-02-14 13:06:00 +0400

    Update shelldashboard.py

--- apps/shelldashboard.py
+++ apps/shelldashboard.py
@@ -1602,7 +1602,7 @@ def update_flag(ctry_name, typeveh_name, base_name):
         title='Potential Headroom',
         barmode='stack',
         margin=go.layout.Margin(
-            l=300,
+            l=350,
             r=50,
             b=100,
             t=100,
