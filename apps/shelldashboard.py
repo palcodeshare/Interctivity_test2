45929ac86bc44b7e9f4f03dcd2bce588c3b25e3b
mokshaxkrodha <nilaykdoshi@gmail.com>
2019-02-14 12:38:53 +0400

    Update shelldashboard.py

--- apps/shelldashboard.py
+++ apps/shelldashboard.py
@@ -11,7 +11,7 @@ import plotly.tools as tls
 from io import StringIO
 import numpy as np
 
-import plotly.plotly import py
+import plotly.plotly as py
 import plotly.graph_objs as go
 
 from app import app
