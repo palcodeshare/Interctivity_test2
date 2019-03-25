3383cb09b0249d94de8c49422e7e2f3f3a16118c
mokshaxkrodha <nilaykdoshi@gmail.com>
2019-03-25 15:14:50 +0400

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_auth
 from app import app
-from apps import shelldashboard,shelldashboard_login,howtouse,notes
+from apps import shelldashboard,howtouse,notes
 import os
 
 app.config['suppress_callback_exceptions']=True
@@ -11,7 +11,7 @@ server = app.server
 server.secret_key = os.environ.get('secret_key', 'secret')
 auth = dash_auth.BasicAuth(
     app,
-    (('Retailaudit','Distributionkpis',),('gfkinternal','gfkoneposdb',),('aajaya','gfkoneposdb37',),)
+    (('Retailaudit','Distributionkpis',),('gfkinternal','gfkoneposdb',),)
 )
 
 app.layout = html.Div([
@@ -45,15 +45,15 @@ def display_page(pathname):
              return notes.layout
         else:
             return shelldashboard.layout
-    elif myauthenticateduser == 'aajaya':
-        if pathname == '/apps/shelldashboard_login':
-             return shelldashboard_login.layout
-        elif pathname == '/apps/howtouse':
-             return howtouse.layout
-        elif pathname == '/apps/notes':
-             return notes.layout
-        else:
-            return shelldashboard_login.layout
+    # elif myauthenticateduser == 'aajaya':
+    #     if pathname == '/apps/shelldashboard_login':
+    #          return shelldashboard_login.layout
+    #     elif pathname == '/apps/howtouse':
+    #          return howtouse.layout
+    #     elif pathname == '/apps/notes':
+    #          return notes.layout
+    #     else:
+    #         return shelldashboard_login.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
