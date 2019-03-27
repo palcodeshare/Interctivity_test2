53e987c3dd065ed49ff8b6283be19888e88df804
mokshaxkrodha <nilaykdoshi@gmail.com>
2019-03-27 09:38:29 +0400

    Update shelldashboard.py

--- apps/shelldashboard.py
+++ apps/shelldashboard.py
@@ -125,7 +125,7 @@ def render_content2(clickData2):
               [Input('shelldbtabs', 'value')])
 
 def render_content(tab):
-    if auth == 'aajaya':
+    if auth._username == 'aajaya':
         # if tab == 'global':
         #     return html.Div([
         #             html.Div([
@@ -820,7 +820,7 @@ def render_content(tab):
         #                 ),
         #             ],className='row'),
         #         ],style={'font-family': 'Calibri Light'})
-    if auth == 'retailaudit':
+    if auth._username == 'retailaudit':
         if tab == 'global':
             return html.Div([
                     html.Div([
