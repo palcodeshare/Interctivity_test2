c25dc2097bf77f5771e633ff367f1a19011740af
mokshaxkrodha <nilaykdoshi@gmail.com>
2019-04-18 12:03:15 +0400

    aa

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ import dash_auth
 import os
 #
 # from flask import send_from_directory
+from rq import Queue
+from worker import conn
+from utils import count_words_at_url
 
+
+
+q = Queue(connection=conn)
+result = q.enqueue(count_words_at_url, 'http://heroku.com')
 
 app = dash.Dash()
 app.config.suppress_callback_exceptions = True
