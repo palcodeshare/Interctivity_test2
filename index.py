27b6a048993ed53014221769d6350413a9965bf2
mokshaxkrodha <nilaykdoshi@gmail.com>
2019-04-18 17:13:45 +0400

    Update index.py

--- index.py
+++ index.py
@@ -53,7 +53,7 @@ app.layout = html.Div([
 
             html.Div(id='shelldbcontent'),
             html.Div([
-                html.Tabs(id='intermediate-value') #dummy value
+                html.Tab(id='intermediate-value') #dummy value
             ],style={'display': 'none'})
 
         ],style={'font-family': 'Calibri Light'},className='ten columns offset-by-one')
